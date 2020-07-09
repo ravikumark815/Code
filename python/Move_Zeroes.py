5ca1bc50e13e1b9510b055e039c9ab80075199c0
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2020-07-09 14:20:56 +0530

    Solution to Move Zeroes problem in Python

---
+++ python/Move_Zeroes.py
@@ -0,0 +1,36 @@
+'''
+Problem Statement:
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Author: https://github.com/ravikumark815
+'''
+import os
+import sys
+
+def moveZeroes(nums):
+    c = 0
+    n = len(nums)
+    i = 0
+    while i<n:
+        if nums[i]==0:
+            c = c+1
+            del nums[i]
+            n = n-1
+            i = i-1
+        i = i+1
+    while c != 0:
+        nums.append(0)
+        c = c-1
+    return nums
+
+if __name__ == "__main__":
+    liStr = input("\nEnter your list of integers separated by commas:")
+    li = liNum = []
+    li = liStr.split(",")
+    for i in li:
+        liNum.append(int(i))
+    print("Result:\t",moveZeroes(liNum))
